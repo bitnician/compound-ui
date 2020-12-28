48aa731c5ac67745905d8672ded34e5d1f5c247e
bitnician <b3hzad@me.com>
2020-12-28 22:45:55 +0330

    specify node version

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,6 @@
 exports.roundToTwo = (num) => {
   return +(Math.round(num + 'e+2') + 'e-2');
 };
-
 exports.capitalizeFirstLetter = (string) => {
   const lower = string.toLowerCase();
   return lower[0].toUpperCase() + lower.slice(1);
